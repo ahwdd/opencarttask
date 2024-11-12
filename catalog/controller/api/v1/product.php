59775f4b57a5448c40994cb1eb65316a4a2a920b
Mohamed Youssef <mydev@my-dev.pro>
2024-11-12 14:04:33 +0200

    task3 update

--- catalog/controller/api/v1/product.php
+++ catalog/controller/api/v1/product.php
@@ -2,37 +2,41 @@
 class ControllerApiV1Product extends Controller
 {
     /**
-    * Show latest products with limit 5 product maximum
-    */
+     * Show latest products with limit 5 product maximum
+     */
     // endpoint: index.php?route=api/vi/product/latestProducts
     public function latestProducts()
     {
+        $total = $this->request->post["total"] ?? null;
+
         $json = [];
         // add header as json response
         $this->response->addHeader("Content-Type: application/json");
 
-        $total = $this->request->post["total"];
-
         // handle exceptions
-        if ($total > 5) {
-            $json['error'] = 'The mac nuber for "total" should be 5.';
+        if (!isset($total) || $total > 6) {
+            $json["error"] = 'The max number for "total" should be 5.';
         }
 
-        if (!empty($json['error'])) {
-            return $this->response->setOutput(json_encode($json))
+        if (!empty($json["error"])) {
+            return $this->response->setOutput(json_encode($json));
         }
 
         // loading the model method
         $this->load->model("catalog/product");
 
-        $latestProducts = $this->model_catalog_produtct->getLatestProducts(
+        $latestProducts = $this->model_catalog_product->getLatestProducts(
             $total
         );
 
-        var_dump($latestProducts);
-        exit;
+        foreach ($latestProducts as $product) {
+            $json["products"][] = [
+                "product_id" => $product["product_id"],
+                "name" => $product["name"],
+                "prict" => $product["price"],
+            ];
+        }
 
-        // send output
         $this->response->setOutput(json_encode($json));
     }
 }
