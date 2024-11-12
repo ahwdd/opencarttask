b5594ae97ae268e45c7ea0a270ccf3cf87e53724
Sameh Bilal <ahwstoretest@server.arabhardware.com>
2024-11-12 10:36:22 +0000

    Initial commit from live server

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://test.ahw.store/admin/');
+define('HTTP_CATALOG', 'https://test.ahw.store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://test.ahw.store/admin/');
+define('HTTPS_CATALOG', 'https://test.ahw.store/');
+
+// DIR
+define('DIR_APPLICATION', '/home2/ahwstoretest/public_html/admin/');
+define('DIR_SYSTEM', '/home2/ahwstoretest/public_html/system/');
+define('DIR_IMAGE', '/home2/ahwstoretest/public_html/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/home2/ahwstoretest/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ahwstoretest_oc');
+define('DB_PASSWORD', 'y~W%[kACuJY@');
+define('DB_DATABASE', 'ahwstoretest_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
